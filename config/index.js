456bc84c3f1574a6eaf8969513b07a9a9a61765e
TuT585 <74639412+TuT585@users.noreply.github.com>
2022-09-25 15:25:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wxa196c5892a6eef66',
+  APP_ID: '',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '38199039359fbf68f122215845f33a13',
+  APP_SECRET: '',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小亚坤',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oblZF6hDZxVSP1iQCc2MSGWkK03Q',
+      id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '3Omp7QIzviZoJyePzfstAMI2OocnfjE4ZNjrqxblPtM',
+      useTemplateId: '',
       // 所在省份
       province: '北京',
       // 所在城市
